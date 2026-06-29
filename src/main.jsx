9f8dc2c438399d794d654c26624390b25a0ad4f4
Raspbsb <unionbjh@naver.com>
2026-06-29 17:07:40 +0900

    feat: 배포용 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 // import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router";
 import "./index.css";
-import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
